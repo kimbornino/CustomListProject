935d2cba0961ba1e38de1239ce495bd0ab74d320
Kimbornino <kimbornino@gmail.com>
2018-09-08 15:47:02 -0500

    created add methods that passed tests

--- CustomListProject/CustomListProject/CustomList.cs
+++ CustomListProject/CustomListProject/CustomList.cs
@@ -9,40 +9,69 @@ namespace CustomListProject
     public class CustomList<T>
     {
         //member variables
-        public T[] array = new T[10];
         private int count;
+        //Why won't this build when I try to use the variable "capactiy" inside of new array index.
+        public int capacity;
+        public T[] array;
 
+        public CustomList()
+        {
+            count = 0;
+            capacity = 10;
+            array = new T[capacity];
+        }
         //methods and properties
-        //this is a property
+       
         public void Add(T input)
         {
-
+            if (count >= capacity)
+            {
+                IncreaseCapacity();
+            }
+            array[count] = input;
+            count = count + 1; 
         }
 
-        /*private public T[] IncreaseCapacity()
+        //increase capacity needs to be able to double the space in array.  Will need to take the new array, copy
+        public void IncreaseCapacity()
         {
-            if (CustomList.Count > array.length) 
-        {
-              return new array
+            T[] tempArray = new T[capacity * 2];
+            for (int i =0; i<capacity; i++)
+            {
+                tempArray[i] = array[i];
+            }
+            array = tempArray;
+            capacity = capacity * 2;
+  
         }
-        } */
-        // Define the indexer to allow client code to use [] notation.
+   
+            
+        // allow array (and list) to use indexer property
         public T this[int i]
         {
-            get { return array[i]; }
-            set { array[i] = value; }
+            get {
+                return array[i];
+            }
+            set {
+                array[i] = value;
+                Console.WriteLine(value);
+            }
         }
+
+        // want to be able to count the total number of items in array.
+        //Trying to figure out how what I'm supposed to "set".  How does this even work?
         public int Count
         {
-            get { return count}
-            set
-            {
-                {
-                    //what IS this even?
-                    //i++;
-                    //return i;
-                }
-            }
+            get { return count; }
+            
+            
+        }
+
+        public object Coun { get; set; }
+
+        public void Remove()
+        {
+
         }
     }
 }
